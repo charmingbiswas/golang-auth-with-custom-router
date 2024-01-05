9633cb9e72fa1fa6822edefdfbafd6165b4d6bae
Riyansh Biswas <charmingbiswas@gmail.com>
2024-01-05 08:25:32 +0530

    Added login and signup pages as separate and implemented switching functionality between then

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
 import { CustomButton } from "../components/Button";
 import { CustomInput } from "../components/Input";
 import "./LoginPage.scss";
+import { Button, Typography } from "@mui/material";
 
 const LoginPage = () => {
+  const [showLogin, setShowLogin] = useState<boolean>(false);
+
+  const changePage = () => {
+    setShowLogin((prev) => !prev);
+  };
+
   const handleClick = async () => {
     const password = document.querySelector("#password") as HTMLInputElement;
     const username = document.querySelector("#username") as HTMLInputElement;
@@ -18,13 +26,33 @@ const LoginPage = () => {
   };
   return (
     <div className="Login-Page">
-      <div className="Login-Card">
-        <CustomInput label="Username" id="username" />
-        <CustomInput label="Password" id="password" />
-        <CustomButton styles={{ width: "10rem" }} onClick={handleClick}>
-          Login
-        </CustomButton>
-      </div>
+      {showLogin ? (
+        <div className="Login-Card">
+          <CustomInput label="Email" id="email" />
+          <CustomInput label="Password" id="password" />
+          <CustomButton styles={{ width: "10rem" }} onClick={handleClick}>
+            Login
+          </CustomButton>
+          <Typography color="white">
+            New Customer?{" "}
+            <Button variant="text" size="medium" onClick={changePage}>
+              Sign Up
+            </Button>
+          </Typography>
+        </div>
+      ) : (
+        <div className="Signup-Card">
+          <CustomInput label="Username" id="username" />
+          <CustomInput label="Email" id="username" />
+          <CustomInput label="Password" id="password" />
+          <CustomButton styles={{ width: "10rem" }} onClick={handleClick}>
+            Signup
+          </CustomButton>
+          <Button variant="text" size="medium" onClick={changePage}>
+            Back To Login
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
